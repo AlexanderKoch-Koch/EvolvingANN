f7988bafe4e683c0909b6477389df00c6307599d
AlexanderKoch-Koch <alexander@4koch.de>
2018-02-17 19:56:51 +0100

    updated file to use Neuron.py and InternalReward.py

--- EvolvingANN.py
+++ EvolvingANN.py
@@ -1,44 +1,50 @@
-from mathai import sigmoid
-from mathai import relu
+from mathai import relu, sigmoid
 import numpy as np
+import gym
+from Neuron import Neuron
+from InternalReward import InternalReward
 
-eta = 0.1
-x = np.array([0.2, 0.8])
-y = np.array([1, 0.2, 0.2])
+episodes = 200
 
-W1 = np.random.random((2, 3))
-W2 = np.random.random((3, 2))
+env = gym.make('CartPole-v0')
+num_inputs = 4
+env.render()
 
+neuron1 = Neuron()
+for i in range(num_inputs):
+    neuron1.add_input()
 
+internalReward = InternalReward(num_inputs)
 
+for episode in range(episodes):
+    is_done = False
+    observation = env.reset()
+    steps = 0
+    while not is_done:
+        env.render()
+        inputs = []
+        if observation[2] > 0:
+            inputs.append(1)
+            inputs.append(0)
+        else:
+            inputs.append(0)
+            inputs.append(1)
 
-for episode in range(9):
-    s1 = np.dot(x, W1)
-    l1 = relu(s1)
+        for i in range(num_inputs):
+            neuron1.set_input(i, observation[i])
 
-    s2 = np.dot(l1, W2)
-    l2 = relu(s2)
-    e = np.square(np.subtract(l1, y))
+        result = neuron1.compute()
+        action = result
+        observation, reward, is_done, info = env.step(action)
+        if is_done:
+            reward = -0.3
+        else:
+            reward = 0.1
 
-    print(e)
+        internal_reward = internalReward.get_internal_reward(observation)
+        neuron1.learn(internal_reward)
+        steps += 1
+        internalReward.add_experience(observation, reward)
 
-    dW2 = np.multiply(np.subtract(l2, y) * relu(s2, derivative=True), l2)
-    np.add(W2, eta * -dW2, out=W2)
-
-    dW = np.multiply(dW2 * relu(s1, derivative=True), W1)
-    np.add(W1, eta * -dW, out=W1)
-
-    if episode % 10 == 0:
-        shape = W1.shape
-        sum_of_weights = np.sum(W1, axis=0)
-        indeces = np.where(sum_of_weights < 0.4)
-        # delete useless neurons
-        W1 = np.delete(W1, indeces, axis=1)
-        print(sum_of_weights)
-
-    if episode % 30 == 0:
-        # add neuron
-        W1_new = np.random.random((2, 1))
-        W1 = np.column_stack([W1, W1_new])
-
-
+    print(steps)
+    internalReward.forget()
