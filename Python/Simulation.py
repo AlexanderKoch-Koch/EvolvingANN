a441b47b445290a7f29d5fd439b03731772c1093
AlexanderKoch-Koch <alexander@4koch.de>
2018-03-10 21:38:55 +0100

    Add function for converting floats to a binary list
    This should help with feeding float values from the environment into the ANN as binary values.

--- Python/Simulation.py
+++ Python/Simulation.py
@@ -3,11 +3,33 @@ import gym
 import time
 
 
+def float_to_binary_list(float_value, precision, len_list):
+    result_list = []
+    print("float_value: " + str(float_value))
+    for i in range(int(len_list/2)):
+        if float_value < -i * precision:
+            result_list.append(1)
+        else:
+            result_list.append(0)
+
+        if float_value > i * precision:
+            result_list.append(1)
+        else:
+            result_list.append(0)
+    print("result_list: " + str(result_list))
+    return result_list
+
+
+def preprocess_inputs(observation):
+    inputs = []
+    inputs += float_to_binary_list(observation[2], 0.05, 10)
+    return inputs
 
 
 env = gym.make('CartPole-v0')
-num_inputs = len(env.reset())
-spikingann.init(0, 1, 1)
+print(preprocess_inputs(env.reset()))
+num_inputs = len(preprocess_inputs(env.reset()))
+spikingann.init(10, num_inputs, 1)
 
 for i in range(1000):
     steps = 0
@@ -15,7 +37,7 @@ for i in range(1000):
     observation = env.reset()
     while not is_done:
         start = time.clock()
-        output = spikingann.think([observation[2]])
+        output = spikingann.think(preprocess_inputs(observation))
         elapsed = time.clock()
         elapsed = elapsed - start
         #print(str(elapsed) + "s")
@@ -34,9 +56,9 @@ for i in range(1000):
         spikingann.reward(reward)
 
         env.render()
-        time.sleep(0.05)
+        time.sleep(0.01)
         steps += 1
 
     spikingann.reset_memory()
 
-    print("Episode " + str(i) + " steps= " + str(steps))
+    # print("Episode " + str(i) + " steps= " + str(steps))
