be5e36df308d821ba62a8a78800e94eed5ef38dd
Alexander Koch <alexander@4koch.de>
2018-03-26 06:31:58 +0000

    Add synapse tagging

--- cuda/Compute.cu
+++ cuda/Compute.cu
@@ -21,13 +21,22 @@ __global__ void compute_neurons(int *d_neuron_outputs, float *d_weighted_sums){
     
     if(d_weighted_sums[neuron] >= THRESHOLD){
         d_neuron_outputs[neuron] = 1;
-        printf("firing :)");
     }else{
         d_neuron_outputs[neuron] = 0;
     }
     
     //reset weighted sum
     d_weighted_sums[neuron] = 0.0;
+}
+
+
+__global__ void tag_synapses(struct Synapse *d_synapses, int *d_neuron_outputs, size_t pitch){
+    int synapse = blockIdx.x*blockDim.x + threadIdx.x;
+    int neuron = blockIdx.y*blockDim.y + threadIdx.y;
+
+    struct Synapse *neuron_array = (struct Synapse *) ((char*)d_synapses + neuron * pitch);
+    neuron_array[synapse].activity *= ACTIVITY_DISCOUNT_FACTOR;
+    neuron_array[synapse].activity += neuron_array[synapse].input * d_neuron_outputs[neuron] * neuron_array[synapse].weight;
 }
 
 
--- cuda/Compute.h
+++ cuda/Compute.h
@@ -7,4 +7,6 @@ __global__ void compute_neurons(int *d_neuron_outputs, float *d_weighted_sums);
 
 __global__ void learn(struct Synapse *d_synapses, float reward, size_t pitch);
 
+__global__ void tag_synapses(struct Synapse *d_synapses, int *d_neuron_outputs, size_t pitch);
+
 #endif
