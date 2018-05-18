d7e4181bf94d73901252725457b80a1db5badea2
Alexander Koch <alexander@4koch.de>
2018-05-18 08:32:40 +0000

    Add t1 synapse initialization
    random presynaptic neurons from d_brain_inputs, d_neuron_outputs and d_t1_neuron_outputs

--- cuda/src/Brain.cu
+++ cuda/src/Brain.cu
@@ -8,9 +8,7 @@
 #include "Hyperparameters.h"
 #include "Parameters.h"
 
-dim3 block_dim(512, 1, 1);
-dim3 grid_dim((NUM_NEURONS + block_dim.x - 1) / block_dim.x);
-curandState_t *d_curand_state;
+
 
 struct Parameters *d_parameters;
 int *d_brain_inputs;
@@ -20,24 +18,29 @@ struct Synapse *d_synapses;
 size_t synapses_pitch;
 int *d_neuron_outputs;
 float *d_weighted_sums;
+dim3 block_dim(512, 1, 1);
+dim3 grid_dim((NUM_NEURONS + block_dim.x - 1) / block_dim.x);
+curandState_t *d_curand_states;
 
 //trainer ANN
 struct Synapse *d_t1_synapses;
 size_t t1_synapses_pitch;
 int *d_t1_neuron_outputs;
 float *d_t1_weighted_sums;
+dim3 t1_block_dim(512, 1, 1);
+dim3 t1_grid_dim((NUM_T1_NEURONS + t1_block_dim.x - 1) / t1_block_dim.x);
+curandState_t *d_t1_curand_states;
 
 
 void init(){
     //mark output start
     printf("#################################################################################################");
-    printf("#################################################################################################");
     printf("synapses memory usage: %zu Bytes", sizeof(struct Synapse) * NUM_NEURONS * NUM_SYNAPSES_PER_NEURON);
     printf("num_neurons: %d block size: %d grid size: %d", NUM_NEURONS, block_dim.x, grid_dim.x);
-    cudaMalloc(&d_curand_state, sizeof(curandState_t) * NUM_NEURONS);
-    init_random_seed<<<grid_dim, block_dim>>>(time(NULL), d_curand_state);
     
     //allocate memory on the device
+    cudaMalloc(&d_curand_states, sizeof(curandState_t) * NUM_NEURONS);
+    cudaMalloc(&d_t1_curand_states, sizeof(curandState_t) * NUM_T1_NEURONS);
     cudaMalloc(&d_parameters, sizeof(struct Parameters));
     cudaMalloc(&d_brain_inputs, sizeof(int) * NUM_INPUTS);
     cudaMalloc(&d_weighted_sums, sizeof(float) * NUM_NEURONS);
@@ -47,11 +50,18 @@ void init(){
     cudaMallocPitch(&d_synapses, &synapses_pitch, NUM_SYNAPSES_PER_NEURON * sizeof(struct Synapse), NUM_NEURONS);
     cudaMallocPitch(&d_t1_synapses, &t1_synapses_pitch, NUM_SYNAPSES_PER_NEURON * sizeof(struct Synapse), NUM_T1_NEURONS);
     
+    //init curand states
+    init_random_seed<<<grid_dim, block_dim>>>(time(NULL), d_curand_states);
+    init_random_seed<<<t1_grid_dim, t1_block_dim>>>(time(NULL), d_t1_curand_states);
+    
+    //copy parameter struct to device
     struct Parameters start_parameters;
     start_parameters.threshold_randomness_factor = THRESHOLD_RANDOMNESS_FACTOR_START;
     cudaMemcpy(d_parameters, &start_parameters, sizeof(struct Parameters), cudaMemcpyHostToDevice);
+    
     // initialize brain
-    init_synapses<<<grid_dim, block_dim>>>(d_synapses, synapses_pitch, d_neuron_outputs, d_brain_inputs, d_curand_state);
+    init_synapses<<<grid_dim, block_dim>>>(d_synapses, synapses_pitch, d_neuron_outputs, d_brain_inputs, d_curand_states);
+    init_t1_synapses<<<t1_grid_dim, t1_block_dim>>>(d_t1_synapses, t1_synapses_pitch, synapses_pitch, d_t1_neuron_outputs, d_neuron_outputs, d_brain_inputs, d_t1_curand_states);
 }
 
 
@@ -65,7 +75,7 @@ int* think(int *inputs){
     cudaDeviceSynchronize();
     
     //compute
-    compute<<<grid_dim, block_dim>>>(d_synapses, d_neuron_outputs, synapses_pitch, d_curand_state, d_parameters);
+    compute<<<grid_dim, block_dim>>>(d_synapses, d_neuron_outputs, synapses_pitch, d_curand_states, d_parameters);
     cudaDeviceSynchronize();
 
     if(iteration_counter % 5000 == 0){
@@ -85,7 +95,7 @@ int* think(int *inputs){
 }
 
 void process_reward(float reward){
-    learn<<<grid_dim, block_dim>>>(d_synapses, reward, synapses_pitch, d_neuron_outputs, d_brain_inputs, d_curand_state);
+    learn<<<grid_dim, block_dim>>>(d_synapses, reward, synapses_pitch, d_neuron_outputs, d_brain_inputs, d_curand_states);
 }
 
 void reset_memory(){
--- cuda/src/BrainSetup.cu
+++ cuda/src/BrainSetup.cu
@@ -39,3 +39,39 @@ __global__ void init_synapses(struct Synapse *d_synapses, size_t pitch, int *d_n
         }
     }
 }
+
+
+__global__ void init_t1_synapses(struct Synapse *d_t1_synapses, size_t t1_pitch, size_t pitch, int *d_t1_neuron_outputs, int *d_neuron_outputs, int *d_brain_inputs, curandState_t *d_curand_state){
+    int neuron = blockIdx.x*blockDim.x + threadIdx.x;
+    
+    if(neuron < NUM_T1_NEURONS){
+        printf("neuron: %d  ", neuron);
+        //set randomly 3/13 of the outputs to 1
+        d_t1_neuron_outputs[neuron] = curand(&d_curand_state[neuron]) % 13 / 10;
+
+        struct Synapse *neuron_array = (struct Synapse *) ((char*)d_t1_synapses + neuron * t1_pitch);
+        
+        for(int synapse = 0; synapse < NUM_SYNAPSES_PER_NEURON; synapse++){
+            //random weight between MIN_START_WEIGHT and MIN_START_WEIGHT + 1
+            float new_weight = curand_uniform(&d_curand_state[neuron]) + MIN_START_WEIGHT;
+            neuron_array[synapse].weight = new_weight;
+            neuron_array[synapse].activity = 0.0;
+            //printf("neuron %d synapse: %d weight %.2f activity: %.2f\n", neuron, synapse, neuron_array[synapse].weight, neuron_array[synapse].activity);
+            //random input
+            int rand_input = curand(&d_curand_state[neuron]) % (NUM_NEURONS + NUM_T1_NEURONS + NUM_INPUTS);
+            if(rand_input < NUM_NEURONS){
+                //connect to neuron from main ANN
+                neuron_array[synapse].p_presynaptic_output = &d_neuron_outputs[rand_input];
+                //printf("connect to neuron from main ANN");
+            }else if(rand_input < NUM_NEURONS + NUM_T1_NEURONS){
+                //connect to T1 neuron
+                neuron_array[synapse].p_presynaptic_output = &d_t1_neuron_outputs[rand_input - NUM_NEURONS];
+                //printf("connect to T1 neuron");
+            }else{
+                //connect to brain input
+                neuron_array[synapse].p_presynaptic_output = &d_brain_inputs[rand_input - (NUM_NEURONS + NUM_T1_NEURONS)];
+                //printf("connect to brain input");
+            }
+        }
+    }
+}
