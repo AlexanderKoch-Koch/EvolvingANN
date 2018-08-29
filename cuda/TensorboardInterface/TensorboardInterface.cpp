f113c58a524609d2874c8d70b26a13d576254337
Alexander Koch <alexander@4koch.de>
2018-08-29 19:30:54 +0000

    Add creating new directory for each run with incremented number if dir already exists

--- cuda/TensorboardInterface/TensorboardInterface.cpp
+++ cuda/TensorboardInterface/TensorboardInterface.cpp
@@ -1,6 +1,7 @@
 #include "TensorboardInterface.h"
 #include <tensorflow/c/c_api.h>
 #include <tensorflow/core/util/events_writer.h>
+#include <tensorflow/core/lib/histogram/histogram.h>
 #include <string>
 #include <iostream>
 #include <sys/types.h>
@@ -44,3 +45,72 @@ void write_scalar(unsigned long step, float scalar, const char *tag)
 }
 
 
+void write_histogram(unsigned long step_arg,  const char *tag) {
+    for (int step = 0; step <27; step++){
+        
+        tensorflow::histogram::Histogram h;
+        for (double  i = 0; i < step; i++)
+          h.Add(i);
+         
+        printf("\nmedian: %f", h.Median());
+        printf(" StandardDeviation: %f", h.StandardDeviation());
+        // convert to proto
+        tensorflow::HistogramProto *hist_proto = new tensorflow::HistogramProto();
+        h.EncodeToProto(hist_proto, true);
+        
+        tensorflow::Event event;
+        event.set_wall_time(std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now().time_since_epoch()).count());
+        event.set_step(step);
+        tensorflow::Summary::Value* summ_val = event.mutable_summary()->add_value();
+        summ_val->set_tag(tag);
+        summ_val->set_allocated_histo(hist_proto);
+        writer->WriteEvent(event);
+    
+    }
+}
+
+
+
+/*int TensorBoardLogger::add_histogram(const string &tag, int step, vector<float> &values) {
+    if (bucket_limits_ == NULL) {
+        generate_default_buckets();
+    }
+
+    vector<int> counts(bucket_limits_->size(), 0);
+    double min = numeric_limits<double>::max();
+    double max = numeric_limits<double>::lowest();
+    double sum = 0.0;
+    double sum_squares = 0.0;
+    for (auto v: values) {
+        auto lb = lower_bound(bucket_limits_->begin(), bucket_limits_->end(), v);
+        counts[lb - bucket_limits_->begin()]++;
+        sum += v;
+        sum_squares += v * v;
+        if (v > max) {
+            max = v;
+        } else if (v < min) {
+            min = v;
+        }
+    }
+
+    auto histo = new HistogramProto();
+    histo->set_min(min);
+    histo->set_max(max);
+    histo->set_num(values.size());
+    histo->set_sum(sum);
+    histo->set_sum_squares(sum_squares);
+    for (size_t i = 0; i < counts.size(); ++i) {
+        if (counts[i] > 0) {
+            histo->add_bucket_limit((*bucket_limits_)[i]);
+            histo->add_bucket(counts[i]);
+        }
+    }
+
+    auto summary = new Summary();
+    auto v = summary->add_value();
+    v->set_node_name(tag);
+    v->set_tag(tag);
+    v->set_allocated_histo(histo);
+
+    return add_event(step, summary);
+}*/
